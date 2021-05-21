ab9667c790d13a098b37c8c9a13641fa9c73c45c
Artemiy Martyanov <artemiy@martyanov.xyz>
2021-05-21 10:42:28 +0300

    init

---
+++ components/ChessBoard.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export const ChessBoard = () => {
+  return (
+    <View style={styles.wrapper}>
+
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: 160,
+    height: 160,
+    backgroundColor: '#000',
+  }
+})
